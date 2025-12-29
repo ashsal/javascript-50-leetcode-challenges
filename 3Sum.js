d657c80beda1635185ce578991136307f1f8ec4a
Ashish Saluja <ashishsaluja1@gmail.com>
2025-12-29 16:18:30 +0100

    3 Sum

--- 3Sum.js
+++ 3Sum.js
@@ -1,25 +1,26 @@
 var threeSum = function (nums) {
   //console.log(nums);
   nums.sort((a, b) => a - b);
-  //const uniqueSet = new Set();
+  //let mySet = new Set();
   //console.log(nums);
   let results = [];
   for (let i = 0; i < nums.length - 2; i++) {
     if (i > 0 && nums[i] === nums[i - 1]) {
       continue;
     }
-
     //console.log(nums[i]);
     let left = i + 1;
     let right = nums.length - 1;
     while (left < right) {
       let sum = nums[i] + nums[left] + nums[right];
       if (sum === 0) {
-        //let key = `${nums[i]}+${nums[left]}+${nums[right]}`;
-        //if (!uniqueSet.has(key)) {
         results.push([nums[i], nums[left], nums[right]]);
-        //uniqueSet.add(key);
-        //}
+        //let key = `${nums[i]}+${nums[left]}+${nums[right]}`;
+
+        /*if (!mySet.has(key)) {
+          results.push([nums[i], nums[left], nums[right]]);
+          mySet.add(key);
+        }*/
 
         left++;
         right--;
@@ -27,7 +28,6 @@ var threeSum = function (nums) {
         while (left < right && nums[left] === nums[left - 1]) {
           left++;
         }
-
         while (left < right && nums[right] === nums[right + 1]) {
           right--;
         }
@@ -41,6 +41,4 @@ var threeSum = function (nums) {
 
   return results;
 };
-
 console.log([-1, 0, 1, 2, -1, -4], threeSum([-1, 0, 1, 2, -1, -4]));
-console.log([-1, 0, 1, 2], threeSum([-1, 0, 1, 2]));
